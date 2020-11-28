7f186575dd1d01bae26a34606b67170c152fcbea
kamaal111 <kamaal@tranzer.com>
2020-11-28 18:33:57 +0100

    Initial commit

---
+++ SwiftyLibExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftyLibExamples
+//
+//  Created by Kamaal Farah on 28/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftyLibExamples/SwiftyLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibExamplesApp.swift
+//  SwiftyLibExamples
+//
+//  Created by Kamaal Farah on 28/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
